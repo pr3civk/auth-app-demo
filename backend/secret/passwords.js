dbbca817f59a77302ec78eaf542f3e24cd628cbb
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2023-11-26 20:55:35 +0100

    Cycki added

--- backend/secret/passwords.js
+++ backend/secret/passwords.js
@@ -1,5 +1,4 @@
 
-
 module.exports = {
   GoogleLogin,
   GooglePass,
--- src/components/content-login.jsx
+++ src/components/content-login.jsx
@@ -1,9 +1,13 @@
 import "../styles/App.css";
+import jpg from "../assets/piersi-1.jpg";
 
 const contentLogin = () => {
   return (
     <>
-      <div className="content-login">Log in completed!</div>
+      <div className="content-login">
+        Log in completed!
+        <img src={jpg} alt="" />
+      </div>
     </>
   );
 };
